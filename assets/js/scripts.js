b80be07f69ede69f4ffd534549142622eb6dffe6
schorij23 <schorij23@gmail.com>
2023-10-25 20:25:19 -0400

    javascript and css

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,63 +16,123 @@ let questionsArray = [
       correct: "C",
     },
   ];
-    let startButton = document.getElementById("start-button");
-    let timerEl = document.getElementById('timer');
+    // Global variables
+    const startButton = document.getElementById("start-button");
+    const timerEl = document.getElementById('timer');
     let maxTime = 120;
     let currentTime = maxTime;
     let timerInterval;
     let currentQuestion = 0;
     let playerGrade = 0; 
 
-   
+    // Function to create a running timer
     function updateTimer() {
         timerEl.textContent = "Time: " + currentTime;
+        // When the timer hits zero end quiz and reset timer
         if (currentTime <= 0) {
           endQuiz();
-          clearInterval(timerInterval);
+          clearTime(timerInterval);
         }
+        // Otherwise deincrament to subtract seconds
         currentTime--;
       }
-      
+      // Add event listener to start the quiz a start timer
       startButton.addEventListener("click", function () {
         timerInterval = setInterval(updateTimer, 1000);
+        // Hide start screen display questions array
         document.getElementById("start-container").classList.add("hide");
         document.getElementById("question-container").classList.remove("hide");
         displayQuestion(questionsArray[currentQuestion]);
     });
   
-      document.querySelectorAll(".btn").forEach(function (btn) {
-      btn.addEventListener("click", function () {
+        // Select all the buttons on the page
+        document.querySelectorAll(".btn").forEach(function (btn) {
+        // Loop the buttons and add event listener
+        btn.addEventListener("click", function () {
+        // Get answer text
         let selectAnswer = this.textContent;
+        // Check if answer is correct
         let isCorrect = selectAnswer.includes(questionsArray[currentQuestion].correct);
-
+        // If correct increase the score
         if (isCorrect) {
           playerGrade++;
+          // Alert when correct
+          displayAlert("Correct", 1000, true);
+        } else {
+            displayAlert("Wrong", 1000, false);
         }
-
-        currentQuestion++;
-
+        setTimeout(function() {
+          // Move to next question
+        currentQuestion++;  
         if(currentQuestion <questionsArray.length) {
-          displayQuestion(questionsArray[currentQuestion]);
-        }else {
-          endQuiz();
+            displayQuestion(questionsArray[currentQuestion]); 
+        } else {
+            endQuiz();
         }
-    });
+        }, 1000); 
+    }); 
   });
+        // Function to end the quiz
+        function endQuiz() {
+        clearInterval(timerInterval);
+        document.getElementById("question-container").classList.add("hide");
+        document.getElementById("score-container").classList.remove("hide");
+        document.getElementById("your-score").textContent = "Your score is: " + playerGrade;
+  }
     
-  // Function to display the question
-  function displayQuestion(qData) {
-    document.getElementById("question").textContent = qData.text;
-    qData.answers.forEach((answer, index) => {
-      document.querySelectorAll(".btn")[index].textContent = answer;
+        
+        function displayAlert (message, duration, isCorrect) {
+            const alertDiv = document.createElement("div");
+            alertDiv.textContent = message;
+            alertDiv.classList.add("alert");            
+
+            if (isCorrect) {
+                alertDiv.classList.add("correct");
+            }
+
+            document.body.appendChild(alertDiv);
+
+            setTimeout (function() {
+                alertDiv.classList.add("hide");
+                document.body.removeChild(alertDiv);
+            }, duration);
+        }
+    
+    // Function to display the question
+    function displayQuestion(qData) {
+        document.getElementById("question").textContent = qData.text;
+        qData.answers.forEach((answer, index) => {
+        document.querySelectorAll(".btn")[index].textContent = answer;
     });  
   }
-     
-  // Function to end the quiz
-  function endQuiz() {
-    clearInterval(timerInterval);
-    document.getElementById("question-container").classList.add("hide");
-    document.getElementById("score-container").classList.remove("hide");
-    document.getElementById("your-score").textContent = "Your score is: " + playerGrade;
+  
+  const submitBtn = document.getElementById("submit-btn");
+    submitBtn.addEventListener("click", function() {
+    document.getElementById("score-container").classList.add("hide");
+    document.getElementById("highscores").classList.remove("hide");
+    viewHighScores();
+  });
+
+  function viewHighScores() {
+    const highScore = [
+        {initials:"", score: "" },
+    ];
+    const highScoreL = document.getElementById("highscores-list");
+    
+    // Clears the existing list
+    highScoreL.innerHTML="";
+    // Display high scores
+    highScore.forEach(score => {
+        const li = document.createElement("li");
+        li.textContent = `${score.initials}: ${score.score}`;
+        highScoreL.appendChild(li);
+    });
+    
   }
+  
+
+  
+  
+  
+  
   
